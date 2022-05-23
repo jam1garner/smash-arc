03d770db866c48c0a231e0711d9e9d497ec1b9e4
jam1garner <8260240+jam1garner@users.noreply.github.com>
2022-05-23 00:57:19 -0400

    Put C bindings behind feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -28,42 +28,44 @@
 //! ## Cargo Features
 //!
 //! * `network` (enabled by default) = Ability to parse the file over the network
-//! * `dir-listing` (enabled by default) = List directories 
+//! * `dir-listing` (enabled by default) = List directories
 //! * `global-hashes` (enabled by default) = Enable a global table for cracking hashes
 //! * `smash-runtime` = Enables features for running under the context of Smash Ultimate itself
 //! (enable Aarch64 crc32 hardware acceleration, enable parsing the Arc from the game's memory
 //! layout)
 //! * `search` = Enable functionality to fuzzy search [`ArcFile`]s
-//! 
+//!
 //! * ZSTD backends
 //!   * `libzstd` - Recommended for use on platforms it builds for
 //!   * `rust-zstd` - Increased portability (Recommended for use on switch)
 //!   * `nozstd` - Provide no zstd backend, panic on ZSTD decompression
 
-mod hash40;
-mod region;
-mod lookups;
 mod filesystem;
+mod hash40;
 mod hash_labels;
-mod zstd_backend;
-mod ffi_bindings;
+mod lookups;
+mod region;
 mod table_indices;
+mod zstd_backend;
+
+#[cfg(feature = "ffi-bindings")]
+mod ffi_bindings;
 
 #[cfg(feature = "search")]
 mod search;
 
+mod arc_file;
 #[cfg(feature = "smash-runtime")]
 mod loaded_arc;
-mod arc_file;
 
 #[cfg(feature = "smash-runtime")]
 pub use loaded_arc::*;
 
 pub use arc_file::*;
 pub use filesystem::*;
-pub use lookups::{ArcLookup, SearchLookup, LookupError};
 pub use hash40::{hash40, Hash40};
-pub use hash_labels::{GLOBAL_LABELS, HashLabels};
+pub use hash_labels::{HashLabels, GLOBAL_LABELS};
+pub use lookups::{ArcLookup, LookupError, SearchLookup};
 pub use table_indices::*;
 
 #[cfg(feature = "serialize")]
